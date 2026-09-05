a7a6e88e94c615158f9143d1d59f5254e78db76a
johnesquillo <johnesquillo286@gmail.com>
2026-09-05 22:37:38 +0800

    Scan a local network for live hosts

---
+++ for-loop-example.sh
@@ -0,0 +1,3 @@
+1.1-Linux-Logs-Explained.txt
+1.1-Linux-Logs-monitoring.txt
+5.1-system-journal.txt
--- scan_network.sh
+++ scan_network.sh
@@ -4,6 +4,7 @@ network="10.82.90."
 
 for host in {1..254}
 do
+
     ip="${network}${host}"
 
     echo "Checking $ip..."
@@ -12,5 +13,14 @@ do
     then
         echo "Host $ip is reachable"
         echo "$ip" >> live_hosts.txt
+
+	ip="${network}${host}"
+
+	echo "Checking $ip..."
+
+	if ping -c 1 -W 1000 "$ip" > /dev/null 2>&1
+then
+	echo "Host $ip  not reachable"
+	echo "$ip" >> live_host.txt
     fi
 done
